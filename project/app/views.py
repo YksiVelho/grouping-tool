a1b6cfdb59ad8d164cca77bed37a5157555187e4
Enni Salmi <enni.salmi@vincit.fi>
2018-11-06 13:47:49 +0200

    Add demo view for making queries

--- project/app/views.py
+++ project/app/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
+from project.credentials import API_TOKEN
+import requests
 
 
 def mainPage(request):
     return HttpResponse("Hello, world. You're at the app main page.")
+
+
+def query(request):
+    contents = requests.get('http://localhost:8000/api/v2/users/2',
+                            headers={'Authorization': f'Token {API_TOKEN}'})
+    return HttpResponse(contents);
